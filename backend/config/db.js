528029e484ac03634fd26b6104e33cf0ed52a81f
Arman <armanjipanjla@gmail.com>
2026-06-01 23:55:34 +0530

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB =async () =>{
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MOngoDB connected : ${conn.connection.host}`)
+        console.log(`MOngoDB connected on URI : ${conn.connection.host}`)
     } catch (error) {
         console.error(`Error ${error.message}`)
         process.exit(1)
